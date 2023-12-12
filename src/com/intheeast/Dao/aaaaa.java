1bb486b5e5668880329264c3717aaf0664385f8c
jjwon03 <cherry102301@gmail.com>
2023-12-12 15:46:37 +0900

    modification main method at master

--- src/com/intheeast/Dao/aaaaa.java
+++ src/com/intheeast/Dao/aaaaa.java
@@ -4,7 +4,9 @@ public class aaaaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("iss53");
+		
+		System.out.print("hello world");
 	}
 
 }
